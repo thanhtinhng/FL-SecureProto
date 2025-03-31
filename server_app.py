955865b9659523d7f1ac6b25c60af24b093199bc
Nguyen Thanh Tinh <ngthanhtinh801@gmail.com>
2025-03-31 12:33:44 +0700

    update

--- server_app.py
+++ server_app.py
@@ -3,7 +3,7 @@
 from flwr.common import Context, ndarrays_to_parameters
 from flwr.server import ServerApp, ServerAppComponents, ServerConfig
 from flwr.server.strategy import FedAvg
-from myfl_flwr_pytorch.task import Net, get_weights
+from task import Net, get_weights
 
 
 def server_fn(context: Context):
